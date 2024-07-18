12900fb42d5fe19542dd7d9afef488e1d1255966
Tobias Dierich <git@tobiasdierich.de>
2024-07-18 09:41:59 +0200

    Ignore scope mismatches when loading offline session for shop

--- src/Lib/ShopifySessionHandler.php
+++ src/Lib/ShopifySessionHandler.php
@@ -2,20 +2,42 @@
 
 namespace Codelayer\LaravelShopifyIntegration\Lib;
 
+use DateTime;
 use Exception;
+use Shopify\Auth\OAuth;
 use Shopify\Auth\Session;
-use Shopify\Utils;
+use Shopify\Context;
 
 class ShopifySessionHandler
 {
     public function getSessionForShopOrThrow(string $shop): Session
     {
-        $session = Utils::loadOfflineSession($shop);
+        $session = $this->loadOfflineSession($shop);
 
         if ($session === null) {
             throw new Exception(
                 message: "No offline session found for shop $shop",
             );
+        }
+
+        return $session;
+    }
+
+    public function sessionIsValid(Session $session): bool
+    {
+        return $session->getAccessToken() &&
+            (! $session->getExpires() || ($session->getExpires() > new DateTime()));
+    }
+
+    private function loadOfflineSession(string $shop, bool $includeExpired = false): ?Session
+    {
+        Context::throwIfUninitialized();
+
+        $sessionId = OAuth::getOfflineSessionId($shop);
+        $session = Context::$SESSION_STORAGE->loadSession($sessionId);
+
+        if ($session && ! $includeExpired && ! $this->sessionIsValid($session)) {
+            return null;
         }
 
         return $session;
